4664964703c5c01f5875e9ae2e36b24689a41857
Maks1842 <maks1842@yandex.ru>
2022-05-16 21:41:24 +0300

    Рефакторинг ЕСИА-ФССП: driver.implicitly_wait на Госуслугах не работает. Применяю явную задержку.

--- esia_fssp_ep.py
+++ esia_fssp_ep.py
@@ -23,41 +23,37 @@ def esia_parsing():
     options = webdriver.ChromeOptions()
     options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36")
 
-    options.add_argument('--headless')     # Бот работает в фоновом режиме
+    # options.add_argument('--headless')     # Бот работает в фоновом режиме
 
     driver = webdriver.Chrome(
         executable_path="/media/maks/Новый том/Python/lessons/linux_puthon_lessons/parsing_lessons/webdriver/chromedriver_linux64/chromedriver",
         options=options
     )
+    driver.implicitly_wait(60)
 
     try:
         driver.get("https://esia.gosuslugi.ru")
         print('start')
-        driver.implicitly_wait(5)
 
         login = driver.find_element(By.ID, "login")
-        login.clear()                                   # Очистка поля от данных
-        driver.implicitly_wait(5)
+        login.clear()
         password = driver.find_element(By.ID, "password")
-        password.clear()                                  # Очистка поля от данных
-        driver.implicitly_wait(5)
+        password.clear()
 
         login.send_keys(esia_di_login)                    # ввод логина и пароля
         password.send_keys(esia_di_password)
         password.send_keys(Keys.ENTER)                    # вход по нажатии Enter, после ввода пароля
         # print('login')
-        driver.implicitly_wait(5)
 
-        # cookies
-        cookies = driver.get_cookies()
-        pickle.dump(cookies, open('cookies_file', 'wb'))
+        # # cookies
+        # cookies = driver.get_cookies()
+        # pickle.dump(cookies, open('cookies_file', 'wb'))
 
         # Переход на страницу ЛК
         xpath_personal_account = 'btn-gotoback'
         driver.find_element(By.CLASS_NAME, xpath_personal_account).click()    # нажатие ссылки
         # print('personal_account')
         time.sleep(5)
-        # driver.implicitly_wait(5)
 
 
 
@@ -77,19 +73,20 @@ def esia_parsing():
             time.sleep(5)
         else:
             # Выбор компании
-            driver.implicitly_wait(10)
             xpath_company = '//div[@class="grid-row"]/button[2]'
+            driver.implicitly_wait(10)
             driver.find_element(By.XPATH, xpath_company).click()
             print('company')
-            time.sleep(5)
-            # driver.implicitly_wait(5)
+            time.sleep(3)
+
 
             # Переход на главную страницу Госуслуг
             xpath_esia = '//*[@id="header"]/div[2]/div/div/div/div/div/div[1]/a'
+            driver.implicitly_wait(10)
             driver.find_element(By.XPATH, xpath_esia).click()
             print('esia')
             # time.sleep(5)
-            # driver.implicitly_wait(5)
+
 
             # Переход в раздел "Услуги"
 
@@ -99,7 +96,6 @@ def esia_parsing():
             driver.find_element(By.XPATH, xpath_category).click()
             print('category')
             # time.sleep(5)
-            # driver.implicitly_wait(5)
 
             # Переход в раздел "Органы власти"
 
@@ -108,7 +104,6 @@ def esia_parsing():
             driver.find_element(By.XPATH, xpath_structure).click()
             print('structure')
             # time.sleep(5)
-            # driver.implicitly_wait(10)
 
             # Переход в раздел "ФССП"
             xpath_fssp = '//div[@class="popular-structure"]/div[2]/div[2]'
@@ -116,23 +111,18 @@ def esia_parsing():
             driver.find_element(By.XPATH, xpath_fssp).click()
             print('fssp')
             # time.sleep(5)
-            # driver.implicitly_wait(10)
 
             # Выбор услуги по предоставлению инфы об ИП
             xpath_service_ip = '//div[@class="structure-passport-list ng-scope"]/a[2]'
-            driver.implicitly_wait(10)
             driver.find_element(By.XPATH, xpath_service_ip).click()
             print('service_ip')
-            # time.sleep(5)
-            # driver.implicitly_wait(10)
+            time.sleep(3)
 
             # Выбор инфы о наличие ИП
             xpath_information_ip = '//li[@data-ng-if="!service.hideAfterFilter"][2]'
-            driver.implicitly_wait(10)
             driver.find_element(By.XPATH, xpath_information_ip).click()
             print('information_ip')
-            time.sleep(10)
-            # driver.implicitly_wait(10)
+            time.sleep(5)
 
             # Проверка наличия шаблона
             page_notifications = driver.page_source
@@ -140,48 +130,42 @@ def esia_parsing():
                 driver.find_element(By.XPATH, '//div[@class="conf-modal__controls"]/lib-button[1]').click()
                 print('проверка шаблона')
                 time.sleep(5)
-                # driver.implicitly_wait(5)
 
             # Выбор "Начать"
-            driver.find_element(By.XPATH, '//div[@class="button-container"]').click()
+            xpath_start = '//div[@class="button-container"]'
+            driver.find_element(By.XPATH, xpath_start).click()
             print('начать')
             time.sleep(5)
-            # driver.implicitly_wait(10)
 
             # Выбор "Нет"
             xpath_no_button = '//epgu-constructor-answer-button[@class="quiz__item"][2]/epgu-cf-ui-long-button[@class="answer-btn"]'
             driver.find_element(By.XPATH, xpath_no_button).click()
             print('no_button')
             time.sleep(5)
-            # driver.implicitly_wait(5)
 
             # Выбор "Верно" ФИО
             xpath_correctly_name = '//div[@class="button-container"]'
             driver.find_element(By.XPATH, xpath_correctly_name).click()
             print('correctly_name')
             time.sleep(5)
-            # driver.implicitly_wait(5)
 
             # Выбор "Верно" организация
             xpath_correctly_user = '//div[@class="button-container"]'
             driver.find_element(By.XPATH, xpath_correctly_user).click()
             print('user')
             time.sleep(5)
-            # driver.implicitly_wait(5)
 
             # Выбор "Верно" адрес регистрации
             xpath_correctly_registration = '//div[@class="button-container"]'
             driver.find_element(By.XPATH, xpath_correctly_registration).click()
             print('registration')
             time.sleep(5)
-            # driver.implicitly_wait(5)
 
             # Выбор "Взыскатель"
             xpath_claimer = '//epgu-constructor-answer-button[@class="quiz__item"][2]/epgu-cf-ui-long-button[@class="answer-btn"]'
             driver.find_element(By.XPATH, xpath_claimer).click()
             print('claimer')
             time.sleep(5)
-            # driver.implicitly_wait(5)
 
             # Выбор "Да"
             xpath_ok_start = '//epgu-constructor-answer-button[@class="quiz__item"][1]/epgu-cf-ui-long-button[@class="answer-btn"]'
@@ -190,13 +174,10 @@ def esia_parsing():
             time.sleep(30)
             current_url = driver.current_url
             print(current_url)
-            # driver.implicitly_wait(60)
 
             with open(f'{today}.txt', 'w') as file:
                 file.write(current_url)
                 file.close()
-
-
 
 
         # Подгрузка всех страниц с ИП
@@ -227,7 +208,6 @@ def esia_parsing():
 
             # Получение ссылок на ИП
             xpath_xx = '//div[@class="shadow-container mb-24"]|//div[@class="shadow-container"]'
-            driver.implicitly_wait(10)
             items = driver.find_elements(By.XPATH, xpath_xx)
             number_items = len(items)
             # print(f'count = {count}: Количество ИП_{number_items}')
@@ -235,13 +215,11 @@ def esia_parsing():
             for i in range(count, number_items):
                 print(f'ИП {count + 1}')
                 xpath_x = f'//div[@class="shadow-container mb-24"][{count + 1}]/h3/a|//div[@class="shadow-container"][1]/h3/a'
-                driver.implicitly_wait(10)
                 driver.find_element(By.XPATH, xpath_x).click()
-                time.sleep(3)
+                time.sleep(2)
                 # print('загрузка ИП')
 
                 page_notifications = driver.page_source
-                driver.implicitly_wait(10)
                 number_ep = driver.find_element(By.XPATH, '//h4[@class="main-title mb-16"]').text
                 print(f'{number_ep = }')
                 if '"gray-text mb-8"' in page_notifications:
@@ -265,7 +243,6 @@ def esia_parsing():
 
                     page2_notifications = driver.page_source
                     if 'Основной долг' in page2_notifications and 'Исполнительский сбор' in page2_notifications and 'Погашенная часть' in page2_notifications:
-                        driver.implicitly_wait(10)
                         main_debt = driver.find_element(By.XPATH, '//div[@class="flex-container flex-column mb-16"][1]/span[2]').text
                         # print(f'{main_debt = }')
                         performance_fee = driver.find_element(By.XPATH, '//div[@class="flex-container flex-column mb-16"][2]/span[2]').text
@@ -273,7 +250,6 @@ def esia_parsing():
                         repaid_debt = driver.find_element(By.XPATH, '//div[@class="flex-container flex-column mb-16"][3]/span[2]').text
                         # print(f'{repaid_debt = }')
                     elif 'Основной долг' in page2_notifications and 'Исполнительский сбор' in page2_notifications and not 'Погашенная часть' in page2_notifications:
-                        driver.implicitly_wait(10)
                         main_debt = driver.find_element(By.XPATH, '//div[@class="flex-container flex-column mb-16"][1]/span[2]').text
                         # print(f'{main_debt = }')
                         performance_fee = driver.find_element(By.XPATH, '//div[@class="flex-container flex-column mb-16"][2]/span[2]').text
@@ -281,12 +257,10 @@ def esia_parsing():
                         repaid_debt = '-'
                         # print(f'{repaid_debt = }')
                     elif 'Основной долг' in page2_notifications and not 'Исполнительский сбор' in page2_notifications and not 'Погашенная часть' in page2_notifications:
-                        driver.implicitly_wait(10)
                         main_debt = driver.find_element(By.XPATH, '//div[@class="flex-container flex-column mb-16"][1]/span[2]').text
                         performance_fee = '-'
                         repaid_debt = '-'
                     elif 'Основной долг' in page2_notifications and not 'Исполнительский сбор' in page2_notifications and 'Погашенная часть' in page2_notifications:
-                        driver.implicitly_wait(10)
                         main_debt = driver.find_element(By.XPATH, '//div[@class="flex-container flex-column mb-16"][1]/span[2]').text
                         # print(f'{main_debt = }')
                         performance_fee = '-'
@@ -298,7 +272,6 @@ def esia_parsing():
                         performance_fee = '-'
                         repaid_debt = '-'
                 else:
-                    driver.implicitly_wait(10)
                     current_debt = driver.find_element(By.XPATH, '//span[@class="amount-owed mr-md-24 mb-24 mb-md-0 gray-amount"]').text
                     # print(f'{current_debt = }')
                     xpath_details_click = '//div[@class="toggle-link mb-24"]'
@@ -312,7 +285,6 @@ def esia_parsing():
                     # print(f'{repaid_debt = }')
 
                 if 'Причина' in page_notifications:
-                    driver.implicitly_wait(10)
                     reason = driver.find_element(By.XPATH, '//div[@class="border-block mb-24"][2]/div[1]/span[2]').text
                     # print(f'{reason = }')
                     footing = driver.find_element(By.XPATH, '//div[@class="border-block mb-24"][2]/div[2]/span[2]').text
@@ -323,25 +295,21 @@ def esia_parsing():
                     footing = driver.find_element(By.XPATH, '//div[@class="border-block mb-24"][2]/div[1]/span[2]').text
                     # print(f'{footing = }')
 
-                driver.implicitly_wait(10)
                 debtor = driver.find_element(By.XPATH, '//div[@class="border-block mb-24"][3]/p').text
                 # print(f'{debtor = }')
                 birthday = driver.find_element(By.XPATH, '//div[@class="border-block mb-24"][3]/div[1]/span[2]').text
                 # print(f'{birthday = }')
                 if 'Место рождения' in page_notifications:
-                    driver.implicitly_wait(10)
                     place_of_birth = driver.find_element(By.XPATH, '//div[@class="border-block mb-24"][3]/div[2]/span[2]').text
                 else:
                     place_of_birth = ''
                 # restriction = driver.find_element(By.XPATH, '//div[@class="border-block mb-24"][4]/div/div/span').text
                 # print(f'{restriction = }')
                 # driver.implicitly_wait(5)
-                driver.implicitly_wait(10)
                 creditor = driver.find_element(By.XPATH, '//div[@class="border-block mb-24"][5]/p').text
                 # print(f'{creditor = }')
                 bailiff = driver.find_element(By.XPATH, '//div[@class="border-block mb-24"][6]/p').text
                 # print(f'{bailiff = }')
-                driver.implicitly_wait(10)
                 bailiff_tel = driver.find_element(By.XPATH, '//div[@class="border-block mb-24"][6]/div[1]/span[2]').text
                 # print(f'{bailiff_tel = }')
                 rosp = driver.find_element(By.XPATH, '//div[@class="border-block mb-24"][6]/div[2]/span[2]').text
@@ -373,8 +341,7 @@ def esia_parsing():
                 })
 
                 driver.back()
-                time.sleep(3)
-                # print(str(count) + '_' + '#' * 40)
+                time.sleep(2)
                 count += 1
             save_file(executive_production)
 
